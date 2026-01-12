7b4468edcbc23764ac51518711c19cbab9362d20
belhub <belhub24@gmail.com>
2026-01-12 19:25:53 +0100

    day03 part2

--- day3/day3.ts
+++ day3/day3.ts
@@ -18,4 +18,38 @@ function part_1() {
   console.log(totalJoltage);
 }
 
-part_1(); // 16812
+// part_1(); // 1681
+
+function part_2() {
+  let totalJoltage = 0;
+
+  input.forEach((line) => {
+    const numbers = line.trim().split("").map(Number);
+    const result: number[] = [];
+    let remaining = 12;
+
+    while (remaining > 0) {
+      const maxSearchEnd = numbers.length - remaining;
+
+      let maxDigit = -1;
+      let maxIndex = -1;
+
+      for (let i = 0; i <= maxSearchEnd; i++) {
+        if (numbers[i] > maxDigit) {
+          maxDigit = numbers[i];
+          maxIndex = i;
+        }
+      }
+
+      result.push(maxDigit);
+      numbers.splice(0, maxIndex + 1);
+
+      remaining--;
+    }
+    totalJoltage += Number(result.join(""));
+  });
+
+  console.log(totalJoltage);
+}
+
+part_2(); //166345822896410
